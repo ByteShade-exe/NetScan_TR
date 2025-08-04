9b3db009e8abb3333249ea3f95139233ca5b1ec4
ByteShade <byteshadebh@gmail.com>
2025-08-04 22:45:53 +0300

    Delete mac_degistirici.py

--- mac_degistirici.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def mac_degistirici():
-    arayuz = input("Ağ arayüzü (örnek: eth0): ")
-    subprocess.run(["ifconfig", arayuz, "down"])
-    subprocess.run(["macchanger", "-r", arayuz])
-    subprocess.run(["ifconfig", arayuz, "up"])
-    print(f"{arayuz} için rastgele MAC adresi atandı.")
